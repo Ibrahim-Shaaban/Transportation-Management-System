4e2a73c6a40d61369d41692632735cf0295ca32e
Ibrahim-Shaaban <ishabaan@damlag.com>
2023-06-19 21:29:35 +0300

    add driver stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      get 'accounts' , to: "accounts#index"
-      post 'accounts', to: "accounts#get_data"
+      resources :drivers
+
     end
   end
 end
