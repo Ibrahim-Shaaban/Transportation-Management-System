b581ffce2429c2222c9ade6f9544060de949fb29
Ibrahim-Shaaban <ishabaan@damlag.com>
2023-06-21 02:36:47 +0300

    import trucks from client

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -4,4 +4,13 @@ class Truck < ApplicationRecord
 
   has_many :assignments
   has_many :drivers, through: :assignments
+
+
+  def self.import_from_api
+    client = TrucksClient.new
+    trucks = client.get_all_trucks
+    trucks.each do |truck_data|
+      Truck.create(name: truck_data['name'], truck_type: truck_data['truck_type'])
+    end
+  end
 end
