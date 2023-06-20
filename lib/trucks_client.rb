2498d1e1cb332e40273b24c1a9afb86af391029e
Ibrahim-Shaaban <ishabaan@damlag.com>
2023-06-21 01:05:58 +0300

    write client to get trucks from api

---
+++ lib/trucks_client.rb
@@ -0,0 +1,27 @@
+require 'httparty'
+class TrucksClient
+  def initialize
+    @api_key = 'illa-trucks-2023'
+    @url = "https://task-api-8etf.onrender.com/api/v1/trucks"
+    @trucks = []
+  end
+
+  def get_all_trucks
+    page = 1
+    total_count = nil
+    total_pages = nil
+
+    while total_pages.nil? || page <= total_pages
+      response = HTTParty.get(@url, query: { page: page }, headers: { 'API_KEY' => @api_key })
+      data = JSON.parse(response.body)
+      total_count ||= response.headers['total-count'].to_i
+      total_pages ||= response.headers['total-pages'].to_i
+
+      @trucks += data
+      page += 1
+
+    end
+
+    @trucks
+  end
+end
